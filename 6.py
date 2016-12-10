cc961f76a4fc880fcbe0b67bada4305c3bc8423f
Daniel Kessler <sinani201@gmail.com>
2016-12-09 22:26:45 -0800

    changed day 6 solution to be nicer and more efficient

--- 6.py
+++ 6.py
@@ -1,7 +1,8 @@
+from collections import Counter
+
 inp = [l.strip() for l in open('i6.txt')]
 
-# Transpose
-msg = list(map(list, zip(*inp)))
-
-print('Part 1:', ''.join(max(set(letterlist), key=letterlist.count) for letterlist in msg))
-print('Part 2:', ''.join(min(set(letterlist), key=letterlist.count) for letterlist in msg))
+# zip(*inp) transposes list of strings
+counts = [Counter(s).most_common() for s in zip(*inp)]
+print('Part 1:', ''.join(count[0][0] for count in counts))
+print('Part 2:', ''.join(count[-1][0] for count in counts))
